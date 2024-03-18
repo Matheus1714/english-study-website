60bdcf789c6abe05c02bb903d84cb1e5afa06986
matheus1714 <matheussilvamartins1714@gmail.com>
2024-03-17 21:24:16 -0300

    config: change oldest design

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,8 @@
 export const SITE_TITLE = 'English Study';
 export const SITE_DESCRIPTION = 'Site para estudo em inglês';
+
+export const AUTHOR_INFORMATION = {
+    fullName: 'Matheus Silva Martins Mota',
+    shortName: 'Matheus Mota',
+    academicName: 'Mota, M. S. M.'
+}
